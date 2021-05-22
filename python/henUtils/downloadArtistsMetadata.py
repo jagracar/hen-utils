0cdc968d50f93830a7a2b08807525dc461d9a867
jagracar <jagracar@gmail.com>
2021-05-22 11:45:21 +0200

    Adds json file with the artists twitter accounts

--- python/henUtils/downloadArtistsMetadata.py
+++ python/henUtils/downloadArtistsMetadata.py
@@ -20,7 +20,7 @@ add_reported_users_information(artists, reported_users)
 
 # Get the account metadata for all the artists
 print_info("Adding artists metadata...")
-batch_size = 250
+batch_size = 50
 from_index = 0
 to_index = min(from_index + batch_size, len(artists))
 counter = 1
@@ -28,7 +28,7 @@ counter = 1
 while True:
     print_info("Processing batch %i: artists %i to %i" % (
         counter, from_index, to_index))
-    add_accounts_metadata(artists, from_index, to_index, sleep_time=2)
+    add_accounts_metadata(artists, from_index, to_index, sleep_time=1)
 
     if to_index == len(artists):
         break
@@ -36,7 +36,7 @@ while True:
     from_index = to_index
     to_index = min(from_index + batch_size, len(artists))
     counter += 1
-    time.sleep(120)
+    time.sleep(60)
 
 # Save the artists information into a json file
 save_json_file("artists.json", artists)
@@ -48,3 +48,11 @@ for walletId, artist in artists.items():
     artists_aliases[walletId] = artist["alias"] if "alias" in artist else ""
 
 save_json_file("artists_aliases.json", artists_aliases)
+
+# Save the artists twitter accounts into a json file
+artists_twitter_accounts = {}
+
+for walletId, artist in artists.items():
+    artists_twitter_accounts[walletId] = artist["twitter"] if "twitter" in artist else ""
+
+save_json_file("artists_twitter_accounts.json", artists_twitter_accounts)
